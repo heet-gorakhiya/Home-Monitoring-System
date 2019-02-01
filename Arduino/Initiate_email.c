01c1836c2904a7bde85fbea8abfa5d9c71fa1a16
Heet Gorakhiya <heet.gorakhiya@gmail.com>
2019-02-01 15:54:06 +0530

    Triggers e-mail
    Sends a signal to a script hosted at any given domain and executes the script.

---
+++ Arduino/Initiate_email.c
@@ -0,0 +1,58 @@
+#include <SPI.h>
+#include <Ethernet.h>
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+char server[] = "summerws.esy.es/Arduino";    // your domain
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(10, 20, 19, 57);
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+void setup() {
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("hello");
+  
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /sendmail.php HTTP/1.1");
+    client.println("Host: www.summerws.esy.es/Arduino");                   //your domain
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+}
+//?text=whadap
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    // do nothing forevermore:
+    while (true);
+  }
+}
